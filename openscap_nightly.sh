131b8783e4320e5a710c94d3dc032edc8333fcc5
Thomas <62696716+beaglemarauder@users.noreply.github.com>
2024-04-18 12:56:57 +0100

    Update openscap_nightly.sh

--- openscap_nightly.sh
+++ openscap_nightly.sh
@@ -6,39 +6,39 @@
 #Variables
 report_directory="/usr/local/share/Openscapreports"
 scan_profile="xccdf_org.ssgproject.content_profile_pci-dss"
-content_profile=/usr/share/xml/scap/ssg/content/ssg-centos8-ds-1.2.xml
+content_profile="/usr/share/xml/scap/ssg/content/ssg-centos8-ds-1.2.xml"
 
 
 #Installing Openscap AND making a reports directory. 
- func1(){
+func1(){
 
-     yum update -y
-     yum upgrade -y
-     sudo yum install openscap-scanner scap-security-guide aide -y
-     ls -l /usr/share/xml/scap/ssg/content/
-     echo "Select your preferred scap security guide for the OS"
-     echo "To select, prepend the previous command with OSCAP INFO and append it with the SSG of your choice"
-     #in this instance it has already been added to the variable, will get Ali to help me write it so that your input with read is used to populate the variable. 
+    yum update -y
+    yum upgrade -y
+    sudo yum install openscap-scanner scap-security-guide aide -y
+    ls -l /usr/share/xml/scap/ssg/content/
+    echo "Select your preferred scap security guide for the OS"
+    echo "To select, prepend the previous command with OSCAP INFO and append it with the SSG of your choice"
+    #in this instance it has already been added to the variable, will get Ali to help me write it so that your input with read is used to populate the variable. 
 
- }
+}
 
 #Make the reports directory if it doesn't exist and get the scanning information
 func2(){
-    mkdir -p {$report_directory}
+    sudo mkdir -p $report_directory
 }
 
 #Performing the assessment
-func2(){
+func3(){
 
-    oscap xccdf eval
-    --profile  {$scan_profile} \
+    sudo oscap xccdf eval \
+    --profile  $scan_profile \
     --results-arf arf.xml \
-    --report {$report_directory}/securityreport_$(hostname)_$(date +%F_%T).txt \
-    {"$content_profile"}
+    --report $report_directory/securityreport_$(hostname)_$(date +%F_%T).txt \
+    "$content_profile"
 }
 
 #Sending the html report as an email to security folk
-# func3(){
+# func4(){
 
 #  recipient="thomaspepper@duck.com"
 #  subject="OpenScap Report"
@@ -49,9 +49,6 @@ func2(){
 
 # }
 
+func1
 func2
 func3
-
-
-
-
