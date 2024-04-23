8566406b23c264fc0c27ca26a790b16ed3e4628c
Thomas <62696716+beaglemarauder@users.noreply.github.com>
2024-04-23 14:03:20 +0100

    Add files via upload

---
+++ openscap_nightly.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+#This script will run the openscap security assessment against the machine as listed in the profile, we'll then automate the hourly run and report using cron.
+#This script assumes that OpenScap is installed but has included a function if not.
+
+#Variables
+report_directory="/usr/local/share/Openscapreports"
+scan_profile="$chosen_ssg"
+content_profile="$chosen_scan_profile"
+
+
+#Some VM's come with the stuff baked in which may cause problems with the content so we need to check if the item's exist first. 
+prechecks(){
+dnf list scap-security-guide
+    if [[ $? -eq 0 ]] then
+        echo "scap-security-guide is not installed"
+    else
+        sudo yum autoremove scap-security-guide -y
+    fi
+dnf list openscap-scanner
+    if [[ $? -eq 0 ]] then
+        echo "openscap-scanner is not installed"
+    else
+        sudo yum autoremove openscap-scanner -y
+    fi
+dnf list aide
+    if [[ $? -eq 0 ]] then
+        echo "aide is not installed"
+    else
+        sudo yum autoremove aide -y
+    fi
+}
+#Installing Openscap AND making a reports directory. 
+func1(){
+
+    yum update -y
+    yum upgrade -y
+    sudo yum install openscap-scanner scap-security-guide aide -y
+    ls -l /usr/share/xml/scap/ssg/content/
+    echo "Select your preferred scap security guide for the OS"
+        read -p "Select your SSG"> $chosen_ssg
+    echo "To select, prepend the previous command with OSCAP INFO and append it with the SSG of your choice"
+        read -p "Select your scan profile" > $chosen_scan_profile
+
+
+}
+
+#Make the reports directory if it doesn't exist and get the scanning information
+func2(){
+    sudo mkdir -p $report_directory
+}
+
+#Performing the assessment
+func3(){
+
+    sudo oscap xccdf eval \
+    --profile  $scan_profile \
+    --results-arf arf.xml \
+    --report $report_directory/securityreport_$(hostname)_$(date +%F_%T).html \
+    "$content_profile"
+}
+
+
+func5(){
+
+    sleep 15m
+}
+
+
+#Sending the html report as an email to security folk
+func5(){
+    #Move to the report directory for the script. 
+    cd /Users/Tom.Pepper/Documents/Bash/AWS_Testing
+    #run the email script using python sdk
+    python3 send_email.py
+}
+
+
+prechecks
+func1
+func2
+func3
+func4
+func5
+
+
+
