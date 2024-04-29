76de8b4f4c89b0d5a544666f835a034a4007ab15
Thomas <62696716+beaglemarauder@users.noreply.github.com>
2024-04-29 16:18:05 +0100

    Update send_mail_output_2.py

--- send_mail_output_2.py
+++ send_mail_output_2.py
@@ -1,6 +1,6 @@
 import boto3
 from botocore.client import Config
-from botocore.exceptions import ClientError
+from botocore.exceptions import NoCredentialsError
 
 def generate_presigned_url(bucket_name, object_name, expiration=3600):
     """
@@ -14,12 +14,13 @@ def generate_presigned_url(bucket_name, object_name, expiration=3600):
     # Create a session using your current credentials
     session = boto3.session.Session()
     # Get the S3 client using the session
+    #s3_client = session.client('s3')
     s3_client = session.client('s3', config=Config(signature_version='s3v4'))
 
     try:
         response = s3_client.generate_presigned_url('get_object',
                                                     Params={'Bucket': bucket_name,
-                                                            'Key': object_name},
+                                                            'Key': object_name,},
                                                     ExpiresIn=expiration)
     except NoCredentialsError:
         print("Credentials not available")
@@ -30,20 +31,14 @@ def generate_presigned_url(bucket_name, object_name, expiration=3600):
 # Usage example
 if __name__ == "__main__":
     bucket = 'openscap-reports'  # TODO: replace with your bucket name
-    key = 'reports/report.html'      # TODO: replace with your object key
+    key = 'reports/securityreport.html'      # TODO: replace with your object key
     url = generate_presigned_url(bucket, key)
 
     if url is not None:
-        print("Generated presigned URL")
+        print("Generated presigned URL: ", url)
 
 
-#ref for this - https://docs.aws.amazon.com/ses/latest/dg/send-an-email-using-sdk-programmatically.html#send-an-email-using-sdk-programmatically-examples
-
-# #references the generate_presigned_url.py file to get the presigned URL
-# result = subprocess.run(['python3', 'generate_presigned_url.py'], stdout=subprocess.PIPE)
-
-# # Capture the pre-signed URL from the output
-# presigned_url = result.stdout.decode().strip()
+############################################################################
 
 
 SENDER = "tompepper20@gmail.com"
